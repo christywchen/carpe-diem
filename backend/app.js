e931ed2643b971360838250ea4b1896f01a4ebe0
Christy Chen <christy.w.chen@gmail.com>
2022-01-05 15:43:19 -0800

    added basic error handling.

--- backend/app.js
+++ backend/app.js
@@ -6,9 +6,11 @@ const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 
 const routes = require('./routes');
-
 const { environment } = require('./config');
+const { ValidationError } = require('sequelize');
+
 const isProduction = environment === 'production';  // check environment is production
+
 
 const app = express();
 
@@ -43,5 +45,36 @@ app.use(
 
 // routers
 app.use(routes);
+
+// error handling
+// for unhandled requests
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+});
+
+// process sequelize errors if error is a sequelize error
+app.use((err, _req, _res, next) => {
+    if (err instanceof ValidationError) {
+        err.errors = err.errors.map((e) => e.message);
+        err.title = 'Validation error';
+    }
+    next(err);
+});
+
+// format errors before returning a json response
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack,
+    });
+});
 
 module.exports = app;
