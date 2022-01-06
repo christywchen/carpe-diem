e166df0686321618bf294d0838f001274c8c290f
Christy Chen <christy.w.chen@gmail.com>
2022-01-06 10:34:08 -0800

    created component for login form.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,16 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import LoginForm from './components/LoginForm';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/login' element={<LoginForm />} />
+    </Routes >
   );
 }
 
