051550bbe9e00e33c513ec5e0d252fcc19870efd
Christy Chen <christy.w.chen@gmail.com>
2022-01-08 23:39:47 -0800

    created thunks for the remaining crud actions for events.

--- frontend/src/store/event.js
+++ frontend/src/store/event.js
@@ -1,4 +1,9 @@
+import { csrfFetch } from "./csrf";
+
 const LOAD_EVENTS = 'events/getEvents';
+const ADD_EVENT = 'event/addEvent';
+const EDIT_EVENT = 'event/editEvent'
+const REMOVE_EVENT = 'event/removeEvent'
 
 // action creators
 export const loadEvents = (events) => {
@@ -8,9 +13,31 @@ export const loadEvents = (events) => {
     }
 }
 
+export const addEvent = (newEvent) => {
+    return {
+        type: ADD_EVENT,
+        newEvent
+    }
+}
+
+export const editEvent = (eventId, updatedEvent) => {
+    return {
+        type: EDIT_EVENT,
+        eventId,
+        updatedEvent
+    }
+}
+
+export const removeEvent = (eventId) => {
+    return {
+        type: REMOVE_EVENT,
+        eventId
+    }
+}
+
 // thunk action creators
 export const getAllEvents = () => async (dispatch) => {
-    const res = await fetch('/api/events');
+    const res = await csrfFetch('/api/events');
 
     if (res.ok) {
         const data = await res.json();
@@ -19,6 +46,49 @@ export const getAllEvents = () => async (dispatch) => {
         throw res;
     }
 };
+
+export const createEvent = (newEvent) => async (dispatch) => {
+    const res = await csrfFetch('/api/events', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newEvent)
+    });
+
+    if (res.ok) {
+        const data = await res.json();
+        dispatch(addEvent(data));
+        return data;
+    } else {
+        throw res;
+    }
+}
+
+export const updateEvent = (eventId, updatedEvent) => async (dispatch) => {
+    console.log(eventId, updatedEvent)
+    const res = await csrfFetch(`/api/events/${eventId}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(updatedEvent)
+    });
+
+    if (res.ok) {
+        const data = await res.json();
+        dispatch(editEvent(data));
+        return data;
+    } else {
+        throw res;
+    }
+}
+
+export const deleteEvent = (eventId) => async (dispatch) => {
+    await csrfFetch(`/api/events/${eventId}`, {
+        method: 'DELETE'
+    });
+}
 
 // initial state
 const initialState = { events: {} }
@@ -34,6 +104,18 @@ const eventReducer = (state = initialState, action) => {
                 events[event.id] = event;
                 return events;
             }, {});
+            return newState;
+        case ADD_EVENT:
+            newState = { ...state };
+            newState.events = { ...newState.events, [action.newEvent.id]: action.newEvent };
+            return newState;
+        case EDIT_EVENT:
+            newState = { ...state };
+            newState.events[action.eventId] = action.updatedEvent;
+            return newState;
+        case REMOVE_EVENT:
+            newState = { ...state };
+            delete newState[action.eventId];
             return newState;
         default:
             return state;
