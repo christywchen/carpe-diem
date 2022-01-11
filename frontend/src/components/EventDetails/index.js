b3d14e8ffdcc373ace6a727a1f6c8df3b99611e6
Christy Chen <christy.w.chen@gmail.com>
2022-01-11 10:58:40 -0800

    modified some of the event related components to include optional chaining in order to avoid issues with redux store not being loaded yet.

--- frontend/src/components/EventDetails/index.js
+++ frontend/src/components/EventDetails/index.js
@@ -1,37 +1,75 @@
 import React, { useEffect } from 'react';
-import { useParams, Navigate, Link } from 'react-router-dom';
+import { useParams, Navigate, Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { getEvent } from '../../store/event';
 
 function EventDetails() {
     const { eventId } = useParams();
-    console.log(typeof eventId)
     const dispatch = useDispatch();
     const event = useSelector(state => state.event.events[eventId]);
-    console.log('event', event)
+    const navigate = useNavigate();
 
     useEffect(() => {
         dispatch(getEvent(eventId));
     }, [dispatch]);
 
-    if (!event) {
-        return <Navigate to='/not-found' />
+    // need to fix redirect issuer
+    // useEffect(() => {
+    //     if (!event) {
+    //         // return navigate('/not-found')
+    //         console.log('redirect here')
+    //         // return <Navigate to='/not-found' />
+    //     }
+    // }, [])
+
+    let locationInfo;
+    if (event?.virtualEvent) {
+        locationInfo = (
+            <>
+                <p>It's online. Check your ticket for the streaming link.</p>
+            </>
+        );
+    } else {
+        if (event?.secretLocation) {
+            locationInfo = (
+                <>
+                    <p>It's a secret! Location will be revealed to attendees later.</p>
+                </>
+            )
+        } else {
+            locationInfo = (
+                <>
+                    <p>ok fine here's the address</p>
+                    <p>{event?.Venue.name}</p>
+                    <p>{event?.Venue.address}</p>
+                    <p>{event?.Venue.city}, {event?.Venue.state} {event?.Venue.zip}</p>
+                </>
+            )
+        }
     }
-
-
-    // const {
-    //     name: venueName,
-
-    // } = event.Venue;
-
-    // const { name: category } = event.Category;
 
     return (
         <>
-            <p>
+            <p> Event Name
                 {event?.name}
-                Event Page
+            </p>
+            <p> Start Time
+                {event?.startTime}
+            </p>
+            <p> End Time
+                {event?.endTime}
+            </p>
+            <p> Event Description
+                {event?.description}
+            </p>
+            <p> Event Description
+            </p>
+            <span>
+                {event && locationInfo}
+            </span>
+            <p>
+                {event?.imageUrl}
             </p>
             <p>
                 <Link to={`/events/${eventId}/edit`} >Edit Event</Link>
