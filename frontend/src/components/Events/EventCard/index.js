5b77ec9e4753e2a7c3b0d93156474c7c8dcc6210
Christy Chen <christy.w.chen@gmail.com>
2022-01-13 15:40:03 -0800

    fixed some bugginess with virtual event vs physical event on cards and seeds.

--- frontend/src/components/Events/EventCard/index.js
+++ frontend/src/components/Events/EventCard/index.js
@@ -6,7 +6,7 @@ import './EventCard.css';
 import { getDateString } from '../../../utils/date-time';
 
 function EventCard({ event }) {
-    const { id, name, startTime, description, categoryId, imageUrl, Category, Venue } = event;
+    const { id, name, startTime, description, categoryId, imageUrl, virtualEvent, Category, Venue } = event;
 
     const backgroundImage = { backgroundImage: `url("${imageUrl}")` }
 
@@ -34,7 +34,7 @@ function EventCard({ event }) {
                     <div className='event__card--date'>{date}, {time}</div>
                     <div className='event__card--location'>
                         <i className="fas fa-map-marker-alt fa-sm event__card--pin" />
-                        {Venue ? (<>{Venue?.city}, {Venue?.state}</>) : (<>Virtual Event</>)}
+                        {!virtualEvent ? (<>{Venue?.city}, {Venue?.state}</>) : (<>Virtual Event</>)}
                     </div>
                     <div className='event__card--description'>{getDescSummary(description)}</div>
                 </div>
