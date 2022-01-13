8cbd27d20e9d39b0e6ae61fcf07f5a0e752413a6
Christy Chen <christy.w.chen@gmail.com>
2022-01-12 17:55:32 -0800

    added event images and fixed image on cards.

--- frontend/src/components/Events/EventCard/index.js
+++ frontend/src/components/Events/EventCard/index.js
@@ -10,21 +10,28 @@ function EventCard({ eventId }) {
     const event = useSelector(state => state.event.events[eventId]);
     let { name, date, categoryId, imageUrl, Category, Venue } = event;
 
+    console.log(imageUrl)
     if (!imageUrl) {
         imageUrl = eventCard;
+    }
+
+    const cardStyling = {
+        backgroundImage: `url('${imageUrl})`
     }
 
     return (
         <>
             <div className='event__card--container'>
-                <div className='event__card--image'>
+                <div className='event__card--image' style={cardStyling}>
                     <Link to={`/events/${eventId}`}>
-                        <img alt='Event Name' src={eventCard} />
                     </Link>
                 </div>
                 <div className='event__card--body'>
                     <div className={`event__card--flag event__card--flag-${categoryId}`}>{Category?.name}</div>
-                    <h3 className='event__card--title'>{name}
+                    <h3 className='event__card--title'>
+                        <Link to={`/events/${eventId}`}>
+                            {name}
+                        </Link>
                     </h3>
                     <div className='event__card--date'>{date}</div>
                     <div className='event__card--location'>
